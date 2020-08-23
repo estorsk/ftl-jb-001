d28af15dfa24044672268aab67ef7ee1b343acdd
Dmitriy <estorsk@yandex.ru>
2020-08-23 17:46:53 +0400

    L001 - Hello world!

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -1,8 +1,12 @@
 package com.company;
 
+import com.company.L001.HelloWorld;
+
 public class Main {
 
     public static void main(String[] args) {
-	// write your code here
+
+        HelloWorld.run();
+
     }
 }
