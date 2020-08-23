ffbdf40c29f1a164151ab76faa5ebff5eaf06872
Dmitriy <estorsk@yandex.ru>
2020-08-23 18:32:47 +0400

    T002 - 5 4 3 2 1

--- src/com/company/L001/HelloWorld.java
+++ src/com/company/L001/HelloWorld.java
@@ -3,8 +3,8 @@ package com.company.L001;
 public class HelloWorld {
 
     public static void run(){
-
-        System.out.println("1 2 3 4 5");
+        
+        System.out.println(5 + " " + 4 + " " + 3 + " " + 2 + " " + 1);
 
     }
 }
